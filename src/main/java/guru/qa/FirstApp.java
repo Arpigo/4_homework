d88b2d1dbcd74c3bf1dfc735ebad98e94b516826
Arpigo <100951285+arpigo@users.noreply.github.com>
2022-05-03 17:49:18 +0300

    add number and operators actions

---
+++ src/main/java/guru/qa/FirstApp.java
@@ -0,0 +1,19 @@
+package guru.qa;
+
+public class FirstApp {
+    public static void main(String[] args){
+        char symbol ='w';
+
+        boolean flag = false;
+
+        byte aByte;
+        short aShort;
+        int aInt;
+        long aLong;
+
+        float aFloat;
+        double aDouble;
+
+
+    }
+}
